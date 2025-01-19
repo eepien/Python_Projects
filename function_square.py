cbb625aac195a7e49461ec2c1a028d3dd5343a73
eepie <eepien@gmail.com>
2025-01-18 23:31:53 -0800

    Added cosmetic changes.

--- function_square.py
+++ function_square.py
@@ -3,5 +3,5 @@ def square(number):
 
 # result = square(3)
 # print(result)
-value = int(input("Enger a number to be squared: "))
+value = int(input("Enter a number to be squared: "))
 print(f" The square of {value} is", square(value))
